f032cfe3f1b559e8428b08887d4125c670c3b5dc
Switch-modder <deolrajjyot@gmail.com>
2026-01-20 12:56:51 -0500

    Compile anims

--- project/buildScripts/dependencies.py
+++ project/buildScripts/dependencies.py
@@ -249,7 +249,7 @@ def extract_files_from_tar(extract_dir, file_types, put_in_subdir=False):
   anim_name_length_mapping = {}
 
   for (dir_path, dir_names, file_names) in os.walk(extract_dir):
-    print("here???")
+    #print("here???")
     # Generate list of all .tar files in/under the directory provided by the caller (extract_dir)
     all_files = map(lambda x: os.path.join(dir_path, x), file_names)
     tar_files = [a_file for a_file in all_files if a_file.endswith('.tar')]
@@ -327,14 +327,14 @@ def get_flatc_dir():
   target_triple = platform_map.get(platform_name)
 
   if target_triple:
-    flatc_dir = os.path.join(THIRD_PARTY_DIR,
+    flatc_dir = os.path.join(EXTERNALS_DIR, 'deps',
                              'flatbuffers', 'host-prebuilts',
                              'current', target_triple, 'bin')
   else: 
     # default
-    flatc_dir = os.path.join(THIRD_PARTY_DIR,
+    flatc_dir = os.path.join(EXTERNALS_DIR, 'deps',
                              'flatbuffers', 'mac', 'Release')
-  print(flatc_dir)
+  #print(flatc_dir)
   return flatc_dir
   
 
@@ -347,16 +347,16 @@ def convert_json_to_binary(json_files, bin_name, dest_dir, flatc_dir):
         tmp_json_files.append(json_dest)
     bin_name = bin_name.lower()
     try:
-        print("i assume")
+        #print("i assume")
         bin_file = binary_conversion.main(tmp_json_files, bin_name, flatc_dir)
-        print("it's here")
+        #print("it's here")
     except StandardError, e:
         print("%s: %s" % (type(e).__name__, e.message))
         # If binary conversion failed, use the json files...
         for json_file in tmp_json_files:
             json_dest = os.path.join(dest_dir, os.path.basename(json_file))
             shutil.move(json_file, json_dest)
-            print("Restored %s" % json_dest)
+            #print("Restored %s" % json_dest)
     else:
         bin_dest = os.path.join(dest_dir, bin_name)
         shutil.move(bin_file, bin_dest)
@@ -519,12 +519,12 @@ def svn_package(svn_dict):
         extract_types = repos[repo].get("extract_types_from_tar", [])
 
         # Move on if the directory already exists
-        if os.path.isdir(loc):
-            print(export_dirname + " already exists!")
-            continue
+        # if os.path.isdir(loc):
+        #     print(export_dirname + " already exists!")
+        #     continue
 
         # Download from the local HTTP server
-        print("Downloading " + export_dirname + "...")
+        # print("Downloading " + export_dirname + "...")
         #if version is not None:
         #    remote_loc = os.path.join(root_url, svn_dict.get("main_folder", ""), repo + '-' + version + ".zip")
         #else:
@@ -573,7 +573,7 @@ def svn_package(svn_dict):
         if extract_types:
             print("Extracting tar files from SVN assets...")
             for sub_dir in sub_dirs:
-                print(sub_dir)
+                #print(sub_dir)
                 put_in_sub_dir = os.path.basename(sub_dir) in UNPACK_INTO_SUBDIR
                 try:
                     anim_name_length_mapping = extract_files_from_tar(os.path.join(loc, sub_dir), extract_types, put_in_sub_dir)
@@ -581,7 +581,7 @@ def svn_package(svn_dict):
                     anim_name_length_mapping = {}
                     print("Failed to unpack one or more tar files in [%s] because: %s" % (sub_dir, e))
                     print(stale_warning)
-                print("possibly..")
+                #print("possibly..")
                 file_stats = get_file_stats(sub_dir)
                 if anim_name_length_mapping:
                     write_animation_manifest(loc, anim_name_length_mapping, additional_files)
